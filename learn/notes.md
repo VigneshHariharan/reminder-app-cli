09a157298b41cff800cac925ec151fccd1738c91
VigneshHariharan <vignesh@Vigneshs-MacBook-Air.local>
2023-07-04 21:01:21 +0530

    WIP: sending one reminder notification with a message

---
+++ learn/notes.md
@@ -0,0 +1,3 @@
+why check for  __name___ as __main__?
+
+This condition is used to check whether the file is executed directly or taken from import
--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from mac_notifications import client
 import schedule
 import time as tm
 
-# input_argument = input("Enter reminder:")
-# input_argument = input_argument.split(" in ");
-# message = input_argument[0];
-# [number, unit] = input_argument[1];
 
 # Dealing only seconds case for scheduling
+def send_notification(message):
+    print('notfication prints',message)
+    # client.create_notification(title='reminder from slack cli', subtitle=message)
+    # return schedule.CancelJob
 
-def send_notification():
-    client.create_notification(title='reminder', subtitle="message")
+def start_schedule(message, argument1, argument2):
+    print('start schedule',message,argument1,argument2,argument2 == 'seconds')
+    if(argument2 == 'seconds'):
+        print('schedule started',int(argument1))
+        schedule.every(5).seconds.do(send_notification)
+        schedule.every(3).seconds.do(send_notification)
 
-schedule.every(5).seconds.do(send_notification)
 
-while True:
-    schedule.run_pending()
-    tm.sleep(1)
+if __name__ == '__main__':
+    input_argument = input("Enter reminder: ");
+    input_argument = input_argument.split(" in ");
+    message = input_argument[0];
+    [argument1, argument2] = input_argument[1].split(" ");
+    print(f"message: {message}, argument1: {argument1}, argument2: {argument2}")
+    start_schedule(message, argument1, argument2)
 
-# if __name__ == "__main__":
-#     client.create_notification(title="Meeting starts now!", subtitle="Team Standup")
